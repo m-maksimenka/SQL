b1f2493c9f0c5f71c3dd9bc0b0b706e254c5c027
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-10-11 20:18:10 +0300

    add solution to task 15

--- Homework_2/homework_2.sql
+++ Homework_2/homework_2.sql
@@ -119,6 +119,16 @@ ON roles_employees.role_id = roles.id
 WHERE roles.role_name LIKE '%Senior%';
 
 -- 15. Вывести зарплаты Java-разработчиков.
+SELECT employees.employee_name, employees_salary.monthly_salary
+FROM employees
+JOIN employees_salary
+ON employees.id = employees_salary.employee_id
+JOIN roles_employees
+ON employees.id = roles_employees.employee_id
+JOIN roles
+ON roles_employees.role_id = roles.id
+WHERE roles.role_name LIKE '%Java developer%';
+
 -- 16. Вывести зарплаты Python-разработчиков.
 -- 17. Вывести имена и зарплаты Junior Python-разработчиков.
 -- 18. Вывести имена и зарплаты Middle JS-разработчиков.
