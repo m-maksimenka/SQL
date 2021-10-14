1c732901f4fffb83f1b295e3dab4475d5970c9d1
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-10-14 21:20:06 +0300

    fix solution to task 17

--- Homework_2/homework_2.sql
+++ Homework_2/homework_2.sql
@@ -145,9 +145,9 @@ SELECT employees.employee_name, employees_salary.monthly_salary
 FROM employees
 FULL JOIN employees_salary
 ON employees.id = employees_salary.employee_id
-FULL JOIN roles_employees
+JOIN roles_employees
 ON employees.id = roles_employees.employee_id
-FULL JOIN roles
+JOIN roles
 ON roles_employees.role_id = roles.id
 WHERE roles.role_name LIKE '%Junior Python developer%';
 
