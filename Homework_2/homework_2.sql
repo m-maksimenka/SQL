c177e4b0aba12b6ca049551fd06346d11859ca37
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-10-14 12:23:48 +0300

    add solution to task 27

--- Homework_2/homework_2.sql
+++ Homework_2/homework_2.sql
@@ -247,6 +247,14 @@ ON roles_employees.role_id = roles.id
 WHERE role_name LIKE '%Middle%';
 
 -- 27. Вывести количество разработчиков.
+SELECT COUNT(employees.employee_name)
+FROM employees
+JOIN roles_employees
+ON employees.id = roles_employees.employee_id
+JOIN roles 
+ON roles_employees.role_id = roles.id
+WHERE role_name LIKE '%developer%'
+
 -- 28. Вывести фонд (сумму) зарплаты разработчиков.
 -- 29. Вывести имена, должности и зарплаты всех специалистов по возрастанию.
 -- 30. Вывести имена, должности и зарплаты всех специалистов, у которых зарплата от 1700 до 2300, по возрастанию.
