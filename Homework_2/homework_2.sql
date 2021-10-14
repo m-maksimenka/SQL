db1fdb357a7e4fee532a44187ae66fcac6a60b83
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-10-14 13:03:27 +0300

    add solution to task 32

--- Homework_2/homework_2.sql
+++ Homework_2/homework_2.sql
@@ -302,3 +302,13 @@ WHERE employees_salary.monthly_salary < 2300
 ORDER BY employees_salary.monthly_salary ASC;
 
 -- 32. Вывести имена, должности и зарплаты всех специалистов, у которых ЗП равна 1100, 1500, 2000, по возрастанию.
+SELECT employees.employee_name, roles.role_name, employees_salary.monthly_salary
+FROM employees
+LEFT JOIN employees_salary
+ON employees.id = employees_salary.employee_id
+LEFT JOIN roles_employees
+ON employees.id = roles_employees.employee_id
+LEFT JOIN roles 
+ON roles_employees.role_id = roles.id
+WHERE employees_salary.monthly_salary IN (1100, 1500, 2000)
+ORDER BY employees_salary.monthly_salary ASC;
