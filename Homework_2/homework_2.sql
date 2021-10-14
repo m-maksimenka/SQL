4c6221afc6f9b313f8cba2bc1f57efd76c3645d7
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-10-14 21:02:15 +0300

    fix solution to task 12

--- Homework_2/homework_2.sql
+++ Homework_2/homework_2.sql
@@ -88,9 +88,9 @@ WHERE roles.role_name LIKE '%Automation QA%';
 -- 12. Вывести имена и зарплаты Junior-специалистов.
 SELECT employees.employee_name, employees_salary.monthly_salary
 FROM employees
-JOIN employees_salary
+FULL JOIN employees_salary
 ON employees.id = employees_salary.employee_id
-JOIN roles_employees
+FULL JOIN roles_employees
 ON employees.id = roles_employees.employee_id
 JOIN roles
 ON roles_employees.role_id = roles.id
