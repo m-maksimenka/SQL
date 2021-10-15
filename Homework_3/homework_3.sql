31d446cbfb152e7bf1ea58219f6f57054f162034
m-maksimenka <maryna-maksimenka@protonmail.com>
2021-10-15 23:09:47 +0300

    modify the salary table

--- Homework_3/homework_3.sql
+++ Homework_3/homework_3.sql
@@ -273,3 +273,7 @@ ADD FOREIGN KEY(supplier_id) REFERENCES suppliers(id);
 -- 6. Обновить таблицу employees. Добавить varchar-поле surname на 50 символов.
 ALTER TABLE employees
 ADD surname varchar(50);
+
+-- 7. Обновить таблицу salary. Добавить varchar-поле currency на 7 символов.
+ALTER TABLE salary
+ADD currency varchar(7);
